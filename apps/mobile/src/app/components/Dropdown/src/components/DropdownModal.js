479240bdc7120ee5a7fb049b281f1da9a782bcd1
Sagar Gupta <sg36.sagargupta26@gmail.com>
2023-01-02 10:44:53 +0530

    removed boilerplate commits

--- apps/mobile/src/app/components/Dropdown/src/components/DropdownModal.js
+++ apps/mobile/src/app/components/Dropdown/src/components/DropdownModal.js
@@ -2,15 +2,13 @@ import React from 'react';
 import {Modal} from 'react-native';
 
 const DropdownModal = ({visible, statusBarTranslucent, children}) => {
-  const defaults = {
-    statusBarTranslucent: statusBarTranslucent || false,
-  };
+  
   return (
     <Modal
       supportedOrientations={['portrait', 'landscape']}
       animationType="none"
       transparent={true}
-      statusBarTranslucent={defaults.statusBarTranslucent}
+      statusBarTranslucent={statusBarTranslucent}
       visible={visible}>
       {children}
     </Modal>
